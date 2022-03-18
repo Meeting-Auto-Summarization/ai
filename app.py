2a05c2a852bf84b9a77c752f651213ab37f6ca43
KKJ <39583312+kkjsw17@users.noreply.github.com>
2022-03-18 14:34:03 +0900

    feat : flask 웹서버 구현 및 모델 로드-예측 함수 구현, 스크립트 POST로 받아와 generate 구현

---
+++ app.py
@@ -0,0 +1,40 @@
+from flask import Flask, request, jsonify
+from flask_cors import CORS
+import torch
+from transformers import PreTrainedTokenizerFast
+from transformers import BartForConditionalGeneration
+import json
+
+app = Flask(__name__)
+
+CORS(app, resources={r'/*': {'origins': '*'}})
+
+tokenizer = PreTrainedTokenizerFast.from_pretrained('gogamza/kobart-summarization')
+model = BartForConditionalGeneration.from_pretrained('gogamza/kobart-summarization')
+model.load_state_dict(torch.load('pytorch_model.bin'))
+
+@app.route('/', methods=['POST', 'GET'])
+def hello_world():
+    if request.method == 'POST':
+        text = json.loads(request.get_data())['contents']
+        return jsonify(predict(text))
+    else:
+        return jsonify('Invalid Method')
+
+def predict(text):
+    summaryList = []
+
+    for i in range(len(text)):
+        summaryList.append([])
+        for j in range(len(text[i])):
+            raw_input_ids = tokenizer.encode(text[i][j])
+            input_ids = [tokenizer.bos_token_id] + raw_input_ids + [tokenizer.eos_token_id]
+
+            summary_ids = model.generate(torch.tensor([input_ids]))
+            summaryList[i].append(tokenizer.decode(summary_ids.squeeze().tolist(), skip_special_tokens=True))
+
+    return summaryList
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
